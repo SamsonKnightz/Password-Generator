b2658f0cd41545d5f5d4aca7d467fe4a23492627
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-01 00:26:03 -0500

    generated random number in consolelog

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//----Global----
+var password = "";
+var passwordLength = 128;
+var possibleSpecial = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function generatePassword() {
-  var password = "";
-  var passwordLength = 12;
-  var possibleSpecial = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-  var letters = "abcdefghijklmnopqrstuvwxyz";
-  var numbers = "1234567890";
-  var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   // YOUR CODE GOES HERE
+  //----Prompts----
   var isUppercase = confirm("Would you like to use uppercase letters?");
   var isLowercase = confirm("Would you like to use lowercase letters?");
   var isSpecial = confirm("Would you like to use special characters?");
   var isNumber = confirm("would you like to use numbers?");
 
+  //----If none was selected----
+if (password >=8 && passwordLength <= 128){
+  alert("Please, select 1 selction of the options")
+}
+
 //----Uppercase?----
-  if (isUppercase = true) {
+  if (isUppercase = confirm) {
     for (var i = 0; i <= passwordLength; i++)
-    random = Math.floor(Math.random() * upLetters.length);
-    console.log("uppercase");
+    var randomNumber = Math.floor(Math.random() * upLetters.length);
+    password += upLetters.substring(randomNumber, randomNumber + 1);
+    console.log(password)
   }
 
 //----Lowercase?----
