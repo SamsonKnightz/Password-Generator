67e277d5b83794fc5c7dc928052dc23802ff7435
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-11-30 20:10:15 -0500

    added characters and if else structure

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // YOUR CODE GOES HERE
+  var characters=
+  `0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ!#$%&(/)*+,-./:;<=>?@[\]^_|~`;
+
+  if (condition) {
+    
+  } else {
+    
+  }
+
+  return;
 }
 
 // Write password to the #password input
