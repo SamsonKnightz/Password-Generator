49a3ff2325aed9c9ecd081737c6a9d29e9fdb33b
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-03 12:48:16 -0500

    organized data with loops

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,6 @@ var generateBtn = document.querySelector("#generate");
 //----Global----
 var password = "";
 var passwordLength = 128;
-var special = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-var letters = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "1234567890";
-var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 
 function generatePassword() {
   // YOUR CODE GOES HERE
@@ -15,39 +10,43 @@ function generatePassword() {
   var isLowercase = confirm("Would you like to use lowercase letters?");
   var isSpecial = confirm("Would you like to use special characters?");
   var isNumber = confirm("would you like to use numbers?");
-
-
-//----Uppercase?----
+  
+  
+  //----Uppercase?----
+  var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   if (isUppercase) {
     for (var i = 0; i <= passwordLength; i++){
-    var random = Math.floor(Math.random() * upLetters.length);
-    password = password + upLetters[random];
+      var random = Math.floor(Math.random() * upLetters.length);
+      password = password + upLetters[random];
     }
   }
-
-//----Lowercase?----
-if (isLowercase) {
-  for (var i = 0; i <= passwordLength; i++){
-  var random = Math.floor(Math.random() * letters.length);
-  password = password + letters[random];
+  
+  //----Lowercase?----
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  if (isLowercase) {
+    for (var i = 0; i <= passwordLength; i++){
+      var random = Math.floor(Math.random() * letters.length);
+      password = password + letters[random];
+    }
   }
-}
-
-//----Special----
-if (isSpecial) {
-  for (var i = 0; i <= passwordLength; i++){
-  var random = Math.floor(Math.random() * special.length);
-  password = password + special[random];
+  
+  //----Special----
+  var special = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+  if (isSpecial) {
+    for (var i = 0; i <= passwordLength; i++){
+      var random = Math.floor(Math.random() * special.length);
+      password = password + special[random];
+    }
   }
-}
-
-//----Number----
-if (isNumber) {
-  for (var i = 0; i <= passwordLength; i++){
-  var random = Math.floor(Math.random() * numbers.length);
-  password = password + numbers[random];
-  }
- return password;
+  
+  var numbers = "1234567890";
+  //----Number----
+  if (isNumber) {
+    for (var i = 0; i <= passwordLength; i++){
+      var random = Math.floor(Math.random() * numbers.length);
+      password = password + numbers[random];
+    }
+    return password;
 }
 console.log(password)
 
