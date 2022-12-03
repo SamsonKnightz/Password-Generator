0c27be82a83baf3fe4557b0fd44682eb7630cc44
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-03 16:15:03 -0500

    linked prompt to possible characters

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var generateBtn = document.querySelector("#generate");
 var password = "";
-var passwordLength = 128;
+var generate = "";
 
 function generatePassword() {
   //--Set prompt to set boundaries of characters chosen
@@ -8,11 +8,11 @@ function generatePassword() {
   numberPrompt = parseInt(numberPrompt);
 
   //----If user does not enter between parameters, function will return with alert.
-  if(numberPrompt <= 8){
-    return alert ("Password has to be more than characters!");
+  if(numberPrompt < 8){
+    return alert ("Password has to be more 8 than characters!");
 }
 //----If user does not enter between parameters, function will return with alert.
-  if(numberPrompt >= 128){
+  if(numberPrompt > 128){
     return alert ("Password has to be less than 128 characters!");
   }
 //----Prompts set to confirm or deny characters chosen
@@ -21,12 +21,10 @@ function generatePassword() {
   var isSpecial = confirm("Would you like to use special characters?");
   var isNumber = confirm("would you like to use numbers?");
 
-
-  
   //----Uppercase?----
   var upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   if (isUppercase) {
-    for (var i = 0; i <= passwordLength; i++){
+    for (var i = 0; i <= numberPrompt; i++){
       var random = Math.floor(Math.random() * upLetters.length);
       password = password + upLetters[random];
     }
@@ -35,7 +33,7 @@ function generatePassword() {
   //----Lowercase?----
   var letters = "abcdefghijklmnopqrstuvwxyz";
   if (isLowercase) {
-    for (var i = 0; i <= passwordLength; i++){
+    for (var i = 0; i <= numberPrompt; i++){
       var random = Math.floor(Math.random() * letters.length);
       password = password + letters[random];
     }
@@ -44,7 +42,7 @@ function generatePassword() {
   //----Special----
   var special = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
   if (isSpecial) {
-    for (var i = 0; i <= passwordLength; i++){
+    for (var i = 0; i <= numberPrompt; i++){
       var random = Math.floor(Math.random() * special.length);
       password = password + special[random];
     }
@@ -53,7 +51,7 @@ function generatePassword() {
   //----Number----
   var numbers = "1234567890";
   if (isNumber) {
-    for (var i = 0; i <= passwordLength; i++){
+    for (var i = 0; i <= numberPrompt; i++){
       var random = Math.floor(Math.random() * numbers.length);
       password = password + numbers[random];
     }
